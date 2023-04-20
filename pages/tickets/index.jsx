21ae291da8989a1231f7ff21b78bf9c66abac761
Ajay Kumar <ajay.ce.kumar@avanade.com>
2023-04-20 16:30:10 +0200

    adding sections

--- pages/tickets/index.jsx
+++ pages/tickets/index.jsx
@@ -2,153 +2,113 @@ import Link from "next/link";
 import { Layout } from "components/users";
 import { DataTable } from "primereact/datatable";
 import { Column } from "primereact/column";
-import { useEffect, useState } from "react";
+import { Button } from "primereact/button";
+import { InputText } from "primereact/inputtext";
+import { useEffect, useRef, useState } from "react";
+import { FilterMatchMode, FilterOperator } from "primereact/api";
+
 export default Index;
 
+const xlsx = require("xlsx");
+const fsaver = require("file-saver");
+
 function Index() {
-  const [customers, setCustomers] = useState([]);
+  const [products, setProducts] = useState([]);
+  const dt = useRef(null);
+  const [globalFilterValue, setGlobalFilterValue] = useState("");
+  const [filters, setFilters] = useState({
+    name: { value: null, matchMode: FilterMatchMode.STARTS_WITH },
+  });
+
+  const cols = [
+    { field: "code", header: "Code" },
+    { field: "name", header: "Name" },
+    { field: "category", header: "Category" },
+    { field: "quantity", header: "Quantity" },
+  ];
+
   useEffect(() => {
-    setCustomers([
+    setProducts([
       {
-        id: 10340,
-        name: "James Butt",
-        country: {
-          name: "Algeria",
-          code: "dz",
-        },
-        company: "Benton, John B Jr",
-        date: "2015-09-13",
-        status: "unqualified",
-        verified: true,
-        activity: 17,
-        representative: {
-          name: "Ioni Bowcher",
-          image: "ionibowcher.png",
-        },
-        balance: 70663,
+        id: "1000",
+        code: "f230fh0g3",
+        name: "Bamboo Watch",
+        description: "Product Description",
+        image: "bamboo-watch.jpg",
+        price: 65,
+        category: "Accessories",
+        quantity: 24,
+        inventoryStatus: "INSTOCK",
+        rating: 5,
       },
       {
-        id: 1032,
-        name: "Jam2es Abv",
-        country: {
-          name: "Algeria",
-          code: "dz",
-        },
-        company: "Benton, John B Jr",
-        date: "2015-09-13",
-        status: "unqualified",
-        verified: true,
-        activity: 17,
-        representative: {
-          name: "Ioni Bowcher",
-          image: "ionibowcher.png",
-        },
-        balance: 70663,
-      },
-      {
-        id: 1001,
-        name: "Ja1mes Butt",
-        country: {
-          name: "Algeria",
-          code: "dz",
-        },
-        company: "Benton, John B Jr",
-        date: "2015-09-13",
-        status: "unqualified",
-        verified: true,
-        activity: 17,
-        representative: {
-          name: "Ioni Bowcher",
-          image: "ionibowcher.png",
-        },
-        balance: 70663,
+        id: "1001",
+        code: "f230fh0g3",
+        name: "ssd Watch",
+        description: "Product Description",
+        image: "bamboo-watch.jpg",
+        price: 65,
+        category: "Accessories",
+        quantity: 24,
+        inventoryStatus: "INSTOCK",
+        rating: 5,
       },
     ]);
-  }, []);
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+  const exportCSV = (selectionOnly) => {
+    dt.current.exportCSV({ selectionOnly });
+  };
+
+  const onGlobalFilterChange = (e) => {
+    const value = e.target.value;
+    let _filters = { ...filters };
+
+    _filters["global"].value = value;
+
+    setFilters(_filters);
+    setGlobalFilterValue(value);
+  };
+
+  const header = (
+    <div>
+      <div className="flex align-items-center justify-content-end gap-2">
+        <Button
+          type="button"
+          icon="fa fa-file-excel"
+          severity="success"
+          rounded
+          onClick={() => exportCSV(false)}
+          data-pr-tooltip="XLS"
+        />
+      </div>
+      <div className="flex justify-content-end">
+        <span className="p-input-icon-left">
+          <i className="pi pi-search" />
+          <InputText
+            value={globalFilterValue}
+            onChange={onGlobalFilterChange}
+            placeholder="Keyword Search"
+          />
+        </span>
+      </div>
+    </div>
+  );
 
   return (
     <Layout>
-      <h1>Tickets</h1>
-      <Link href="/users/add" className="btn btn-sm btn-success mb-2">
-        Add Ticket
-      </Link>
-      <DataTable
-        value={customers}
-        paginator
-        rows={10}
-        paginatorTemplate="FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink CurrentPageReport RowsPerPageDropdown"
-        rowsPerPageOptions={[10, 25, 50]}
-        dataKey="id"
-        selectionMode="checkbox"
-        filterDisplay="menu"
-        emptyMessage="No tickets found."
-        currentPageReportTemplate="Showing {first} to {last} of {totalRecords} entries"
-      >
-        <Column
-          selectionMode="multiple"
-          headerStyle={{ width: "3rem" }}
-        ></Column>
-        <Column
-          field="name"
-          header="Name"
-          sortable
-          filter
-          filterPlaceholder="Search by name"
-          style={{ minWidth: "14rem" }}
-        />
-        <Column
-          field="country.name"
-          header="Country"
-          filterField="country.name"
-          style={{ minWidth: "14rem" }}
-          filter
-          filterPlaceholder="Search by country"
-        />
-        <Column
-          header="Agent"
-          sortField="representative.name"
-          filterField="representative"
-          showFilterMatchModes={false}
-          filterMenuStyle={{ width: "14rem" }}
-          style={{ minWidth: "14rem" }}
-          filter
-        />
-        <Column
-          field="date"
-          header="Date"
-          filterField="date"
-          dataType="date"
-          style={{ minWidth: "12rem" }}
-          filter
-        />
-        <Column
-          field="balance"
-          header="Balance"
-          dataType="numeric"
-          style={{ minWidth: "12rem" }}
-          filter
-        />
-        <Column
-          field="status"
-          header="Status"
-          sortable
-          filterMenuStyle={{ width: "14rem" }}
-          style={{ minWidth: "12rem" }}
-          filter
-        />
-        <Column
-          field="activity"
-          header="Activity"
-          sortable
-          showFilterMatchModes={false}
-          style={{ minWidth: "12rem" }}
-          filter
-        />
-        <Column
-          headerStyle={{ width: "5rem", textAlign: "center" }}
-          bodyStyle={{ textAlign: "center", overflow: "visible" }}
-        />
-      </DataTable>
+      <div className="card">
+        <DataTable
+          ref={dt}
+          value={products}
+          header={header}
+          tableStyle={{ minWidth: "50rem" }}
+        >
+          {cols.map((col, index) => (
+            <Column key={index} field={col.field} header={col.header} />
+          ))}
+        </DataTable>
+      </div>
     </Layout>
   );
 }
