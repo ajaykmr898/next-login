4f970dd450c38b17e2a747838c784a075beab609
Ajay Kumar <ajay.ce.kumar@avanade.com>
2023-04-28 13:08:43 +0200

    upload page added

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   publicRuntimeConfig: {
     isLocal: process.env.MONGODB_URI.includes("italy"),
+    platform: "industicketmanager|1|->!",
   },
 };
 
