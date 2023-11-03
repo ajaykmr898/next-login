0d87226ac59b39e187b0c8bca789683a5acb1a7a
Ajay Kumar <ajay.ce.kumar@avanade.com>
2023-11-03 09:55:12 +0100

    adding backup

---
+++ bkupdb.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+if [ -f .env ]; then
+  export $(echo $(cat .env | sed 's/#.*//g'| xargs) | envsubst)
+fi
+
+echo Tickets downloading...
+curl --location --request POST "https://eu-central-1.aws.data.mongodb-api.com/app/data-jukku/endpoint/data/v1/action/find" --header "Content-Type: application/json" --header "Access-Control-Request-Headers: *" --header "api-key: $MONGODB_API" --data-raw '{"collection":"tickets", "database":"test", "dataSource":"Cluster0"}' > tickets.json
+echo Operations downloading...
+curl --location --request POST "https://eu-central-1.aws.data.mongodb-api.com/app/data-jukku/endpoint/data/v1/action/find" --header "Content-Type: application/json" --header "Access-Control-Request-Headers: *" --header "api-key: $MONGODB_API" --data-raw '{"collection":"operations", "database":"test", "dataSource":"Cluster0"}' > operations.json
