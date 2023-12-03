b14a6af5d56bb81a2d7efad020d30c823caf9141
Ajay Kumar <ajay.ce.kumar@avanade.com>
2023-12-03 15:35:12 +0100

    adding curl command

--- bkupdb.sh
+++ bkupdb.sh
@@ -4,6 +4,6 @@ if [ -f .env ]; then
 fi
 
 echo Tickets downloading...
-curl --location --request POST "https://eu-central-1.aws.data.mongodb-api.com/app/data-jukku/endpoint/data/v1/action/find" --header "Content-Type: application/json" --header "Access-Control-Request-Headers: *" --header "api-key: $MONGODB_API" --data-raw '{ "collection":"tickets", "database":"test", "dataSource":"Cluster0", "skip": 0, "limit": 0 }' > tickets.json
+curl --location --request POST "https://eu-central-1.aws.data.mongodb-api.com/app/data-jukku/endpoint/data/v1/action/find" --header "Content-Type: application/json" --header "Access-Control-Request-Headers: *" --header "api-key: $MONGODB_API" --data-raw '{ "collection":"tickets", "database":"test", "dataSource":"Cluster0", "skip": 0, "limit": 50000 }' > tickets.json
 echo Operations downloading...
-curl --location --request POST "https://eu-central-1.aws.data.mongodb-api.com/app/data-jukku/endpoint/data/v1/action/find" --header "Content-Type: application/json" --header "Access-Control-Request-Headers: *" --header "api-key: $MONGODB_API" --data-raw '{"collection":"operations", "database":"test", "dataSource":"Cluster0"}' > operations.json
+curl --location --request POST "https://eu-central-1.aws.data.mongodb-api.com/app/data-jukku/endpoint/data/v1/action/find" --header "Content-Type: application/json" --header "Access-Control-Request-Headers: *" --header "api-key: $MONGODB_API" --data-raw '{ "collection":"operations", "database":"test", "dataSource":"Cluster0", "skip": 0, "limit": 50000}' > operations.json
