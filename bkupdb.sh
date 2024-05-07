ad505d629f055d58f75eeaf534901094b0fe67b1
Ajay Kumar <ajay.ce.kumar@avanade.com>
2024-05-07 09:20:53 +0200

    adding backup

--- bkupdb.sh
+++ bkupdb.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
-if [ -f .env ]; then
-  export $(echo $(cat .env | sed 's/#.*//g'| xargs) | envsubst)
-fi
+TODAY=$(date +%F_%H-%M-%S)
+mkdir -p managerBackups
+cd managerBackups
 
-echo Tickets downloading...
-curl --location --request POST "https://eu-central-1.aws.data.mongodb-api.com/app/data-jukku/endpoint/data/v1/action/find" --header "Content-Type: application/json" --header "Access-Control-Request-Headers: *" --header "api-key: $MONGODB_API" --data-raw '{ "collection":"tickets", "database":"test", "dataSource":"Cluster0", "skip": 0, "limit": 50000 }' > tickets.json
-echo Operations downloading...
-curl --location --request POST "https://eu-central-1.aws.data.mongodb-api.com/app/data-jukku/endpoint/data/v1/action/find" --header "Content-Type: application/json" --header "Access-Control-Request-Headers: *" --header "api-key: $MONGODB_API" --data-raw '{ "collection":"operations", "database":"test", "dataSource":"Cluster0", "skip": 0, "limit": 50000}' > operations.json
+for collection in "${COLLECTIONS[@]}"; do
+  echo "$collection downloading..."
+  curl --location --request POST "$URL" \
+    --header "Content-Type: $TYPE" \
+    --header "Access-Control-Request-Headers: *" \
+    --header "api-key: $KEY" \
+    --data-raw "{ \"collection\": \"$collection\", \"database\": \"test\", \"dataSource\": \"Cluster0\", \"skip\": 0, \"limit\": 50000 }" \
+     > "$collection"_"$TODAY.json"
+done
