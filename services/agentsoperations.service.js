fdee93b0d2ab81e966bbc97f6f57bcf7473bff96
Ajay Kumar <ajay.ce.kumar@avanade.com>
2024-05-13 16:37:32 +0200

    adding tickets in agents transfer

--- services/agentsoperations.service.js
+++ services/agentsoperations.service.js
@@ -18,7 +18,7 @@ async function getAll(filters) {
   let tickets = [];
   ticketsT.map((t) => {
     if (
-      (filters.agent === null && t.agentCost !== "0") ||
+      (filters.agent === null && t.agentCost.trim() !== "0") ||
       filters.agent === t.agentId
     ) {
       tickets.push(t);
